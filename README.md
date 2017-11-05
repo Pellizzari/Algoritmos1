3c317e7bef167076cf07505398819185e3c31618
Pellizzari <giorgio.pellizzar.14@sansano.usm.cl>
2017-11-05 07:40:31 -0300

    agregar queue, bfs agregado con errores

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Instrucciones
-
-```
-make
-./main < small-input.dat
+LINEA 153 SEG FAULT!!!!!
+LINEA 196 y 197 SON LOS ELEMENTOS MOSTRADOS POR CONSOLA!!!!
 ./main < medium-input.dat
 ```
--- main.c
+++ main.c
@@ -1,50 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 2000000
 
-struct node
-{
+//implementacion de cola-------------------------------
+struct queue {
+    int items[SIZE];
+    int front;
+    int rear;
+};
+
+struct queue* createQueue() {
+    struct queue* q = malloc(sizeof(struct queue));
+    q->front = -1;
+    q->rear = -1;
+    return q;
+}
+
+
+int isEmpty(struct queue* q) {
+    if(q->rear == -1)
+        return 1;
+    else
+        return 0;
+}
+
+void enqueue(struct queue* q, int value){
+    if(q->rear == SIZE-1)
+        printf("\nQueue is Full!!");
+    else {
+        if(q->front == -1)
+            q->front = 0;
+        q->rear++;
+        q->items[q->rear] = value;
+    }
+}
+
+int dequeue(struct queue* q){
+    int item;
+    if(isEmpty(q)){
+        printf("Queue is empty");
+        item = -1;
+    }
+    else{
+        item = q->items[q->front];
+        q->front++;
+        if(q->front > q->rear){
+            printf("Resetting queue");
+            q->front = q->rear = -1;
+        }
+    }
+    return item;
+}
+
+void printQueue(struct queue *q) {
+    int i = q->front;
+
+    if(isEmpty(q)) {
+        printf("Queue is empty");
+    } else {
+        printf("\nQueue contains \n");
+        for(i = q->front; i < q->rear + 1; i++) {
+                printf("%d ", q->items[i]);
+        }
+    }
+}
+
+//-----------------------------------------------------------
+
+struct node{
     int vertex;
     struct node* next;
 };
-struct node* createNode(int);
 
-struct Graph
-{
+struct Graph{
     int numVertices;
     struct node** adjLists;
+    int* visited;
 };
 
-struct Graph* createGraph(int vertices);
-void addEdge(struct Graph* graph, int src, int dest);
-void printGraph(struct Graph* graph);
-
-
-
-struct node* createNode(int v)
-{
+struct node* createNode(int v){
     struct node* newNode = malloc(sizeof(struct node));
     newNode->vertex = v;
     newNode->next = NULL;
     return newNode;
 }
 
-
-struct Graph* createGraph(int vertices)
-{
+struct Graph* createGraph(int vertices){
     struct Graph* graph = malloc(sizeof(struct Graph));
     graph->numVertices = vertices;
 
     graph->adjLists = malloc(vertices * sizeof(struct node*));
+    graph->visited = malloc(vertices * sizeof(int));
 
     int i;
-    for (i = 0; i < vertices; i++)
+    for (i = 0; i < vertices; i++){
         graph->adjLists[i] = NULL;
+        graph->visited[i]=0;
+    }
 
     return graph;
 }
-
-void addEdge(struct Graph* graph, int src, int dest)
-{
+void addEdge(struct Graph* graph, int src, int dest){
     // Add edge from src to dest
     struct node* newNode = createNode(dest);
     newNode->next = graph->adjLists[src];
@@ -56,8 +112,7 @@ void addEdge(struct Graph* graph, int src, int dest)
     graph->adjLists[dest] = newNode;
 }
 
-void printGraph(struct Graph* graph)
-{
+void printGraph(struct Graph* graph){
     int v;
     for (v = 0; v < graph->numVertices; v++)
     {
@@ -71,7 +126,41 @@ void printGraph(struct Graph* graph)
         printf("\n");
     }
 }
-//#include "bfs.h"
+
+//sort of bfs-------------------------------------------------
+
+void bfs(struct Graph* graph, int startVertex) {
+    struct queue* q = createQueue();
+    graph->visited[startVertex] = 1;
+    enqueue(q, startVertex);
+
+    while(!isEmpty(q)){
+        printQueue(q);
+        int currentVertex = dequeue(q);
+        printf("Visited %d\n", currentVertex);
+
+       struct node* temp = graph->adjLists[currentVertex];
+
+       while(temp) {
+            int adjVertex = temp->vertex;
+            printf("%d\n",adjVertex);
+
+            if(graph->visited[adjVertex] == 0){
+                graph->visited[adjVertex] = 1;
+                enqueue(q, adjVertex);
+                printf("%d\n",adjVertex);
+            }
+            //AQUI HAY SEG FAULT!!!!!!!!!!!!!!!!!!!!!!!
+            temp = temp->next;
+       }
+    }
+}
+
+//-------------------------------------
+
+
+
+
 
 // Driver program to test above functions
 int main()
@@ -83,8 +172,8 @@ int main()
 	//printf("Ingrese cantidad usuarios:\n");
 	scanf("\n %d[^\n]",&N);
 
+  //usuarios posee la fobia de cada usuario
 	int usuarios[N];
-
 	while(i<N){
 		//printf("Ingrese fobia del usuario %d:\n",i+1);
 		scanf("\n %d[^\n]",&entrada1);
@@ -92,24 +181,20 @@ int main()
 		i++;
 	}
 
-	i = 0;
+	i = 0 ;
 
 	//printf("Ingrese cantidad relaciones:\n");
 	scanf("\n %d[^\n]",&M);
 
 	struct Graph* graph = createGraph(N);
-	//struct subset* subsetFinal = (struct subset*) malloc( N * sizeof(struct subset) );
-
 	while(i<M){
 		//printf("Ingrese relacion %d:\n",i+1);
 		scanf("\n%d %d[^\n]", &entrada1, &entrada2);
-		addEdge(graph, entrada1-1, entrada2-1);
+		addEdge(graph, entrada1, entrada2);
 		i++;
 	}
-
-
-	//isCycle(graph, usuarios);
 	printGraph(graph);
+  bfs(graph,1);
 
     return 0;
 }
